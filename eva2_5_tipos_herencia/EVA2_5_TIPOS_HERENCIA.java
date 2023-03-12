c3e5f91ac4c8b6f3f8b6cb26d100bd7b1eb8809f
EduardoMunozN <124702009+EduardoMunozN@users.noreply.github.com>
2023-03-12 17:18:40 -0600

    Add files via upload

---
+++ eva2_5_tipos_herencia/EVA2_5_TIPOS_HERENCIA.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva2_5_tipos_herencia;
+
+/**
+ *
+ * @author eduar
+ */
+public class EVA2_5_TIPOS_HERENCIA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+class SuperClase{
+    
+}
+class OtraSuperClase{
+    //NO SE PUEDE HEREDAR DE DOS O MÁS CLASES
+}
+   //EXTENDS SOLO FUNCIONA PARA UNA ÚNICA CLASE
+class BaseClase extends SuperClase{
+    
+}
